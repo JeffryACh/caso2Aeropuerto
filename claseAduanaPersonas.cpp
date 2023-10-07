735bf85be67f2abea04df8dab52e44b275012223
Jeffry Araya Ch <jeffry.bilps@gmail.com>
2023-10-07 16:05:00 -0600

    revisar la retencion

--- claseAduanaPersonas.cpp
+++ claseAduanaPersonas.cpp
@@ -12,25 +12,29 @@ using namespace std;
 class AduanaPersonas : public Zonas{
     // Atributos específicos de AduanaPersonas
 private:
-    Pasajero *pasajero;
+    int cantidadPersonasRevisadas;
+    vector<Pasajero> pasajeros;
     int capacidad;
-    bool revisionPersona; // Indica si se ha realizado la revisión de la persona
 
 public:
     // Constructor
     AduanaPersonas(int capacidad, bool revisionPersona, Pasajero pasajero, int cantidadPersonas, int cantidadMaletas) : Zonas(capacidad, cantidadPersonas, cantidadMaletas) {
-        this->revisionPersona = revisionPersona;
-        this->pasajero = new Pasajero(pasajero);
-        this->revisionPersona = false;
+        this->pasajeros.push_back(pasajero);
     }
 
     // Métodos específicos de AduanaPersonas
-    bool haSidoRevisada() {
-        return revisionPersona;
+    bool haSidoRevisada(Pasajero pasajero) {
+        for (int i = 0; i < this->pasajeros.size(); i++) {
+            if (this->pasajeros[i].getPasaporte().codigo == pasajero.getPasaporte().codigo) {
+                return realizarRevision(pasajero);
+            }
+        }
     }
 
     bool realizarRevision(Pasajero pPasajero) {
         return validarCodigoPasaporte(pPasajero.getPasaporte().codigo, pPasajero.getNacionalidad(), pPasajero.getNombre(), pPasajero.getEdad());
     }
+
+
 };
 
